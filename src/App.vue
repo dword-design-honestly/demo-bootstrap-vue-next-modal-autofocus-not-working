6dd04dbaf05370f4c5a84d353c83dc34f47b0169
Sebastian Landwehr <153498584+dword-design-honestly@users.noreply.github.com>
2024-07-17 13:27:16 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-button @click="isShown = true" />
+  <b-modal id="isShown" v-model="isShown" :auto-focus="false">
+    <b-form-input autofocus />
+  </b-modal>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isShown = ref(false);
+</script>
